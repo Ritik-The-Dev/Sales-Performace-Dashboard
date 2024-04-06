f00ae58264cd3152908a29bde9cd4a6dac961958
Ritik-the-dev <joshiritik865@gmail.com>
2024-04-06 22:55:51 +0530

    loading

--- client/src/layout/BaseLayout.jsx
+++ client/src/layout/BaseLayout.jsx
@@ -1,18 +1,34 @@
 import { Outlet } from "react-router-dom";
 import { Sidebar } from "../components";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSetRecoilState } from "recoil";
 import axios from "axios";
 import { salesData } from "../Recoil/Data";
+import ClipLoader from "react-spinners/ClipLoader";
 import { GetSalesData } from "../assets/Api/Api";
 
+const override = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
+
 const BaseLayout = () => {
+  const [Loading, setLoading] = useState(false);
+  let [color, setColor] = useState("#ffffff");
   const setSalesData = useSetRecoilState(salesData);
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await getSalesData();
-      setSalesData(data);
+      try {
+        setLoading(true);
+        const data = await getSalesData();
+        setSalesData(data);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+      }
     };
     fetchData();
   }, []);
@@ -21,7 +37,22 @@ const BaseLayout = () => {
     <main className="bg-[#fafafa] min-h-[100vh] overflow-hidden">
       <Sidebar />
       <div className="bg-[#fafafa] min-h-[100vh] lg:p-14 md:p-10 p-5  lg:ml-64 ml-0 md:ml-10">
-        <Outlet />
+        {Loading ? (
+          <div className="sweet-loading flex items-center justify-center flex-col h-[80vh]">
+            <ClipLoader
+              color={color}
+              s
+              loading={Loading}
+              cssOverride={override}
+              size={150}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+            <h1 className="text-center mt-5">Loading Charts...</h1>
+          </div>
+        ) : (
+          <Outlet />
+        )}
       </div>
     </main>
   );
